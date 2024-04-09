ae15c0b997dd884c534a0250ee1a034d3124bcbb
Thomas Roth <thomas.roth@aon.at>
2024-04-09 17:36:22 +0200

    code function for calculation and display result
    added event listener to equal button and wrote
    an arrow function to call operate funcion and
    display the result

--- scripts.js
+++ scripts.js
@@ -16,37 +16,54 @@ function divide (a, b) {
 
 let numberFirst = 0;
 let numberSecond = 0;
-let operator;
+let operator = "";
 
 let display = document.querySelector("#display");
+let operationFromDisplay;
 
-let numberInput = [
-    {num: document.querySelector("#btn0"), value: 0},
-    {num: document.querySelector("#btn1"), value: 1},
-    {num: document.querySelector("#btn2"), value: 2},
-    {num: document.querySelector("#btn3"), value: 3},
-    {num: document.querySelector("#btn4"), value: 4},
-    {num: document.querySelector("#btn5"), value: 5},
-    {num: document.querySelector("#btn6"), value: 6},
-    {num: document.querySelector("#btn7"), value: 7},
-    {num: document.querySelector("#btn8"), value: 8},
-    {num: document.querySelector("#btn9"), value: 9},
+const equalInput = document.querySelector("#btnEqual");
+const clearInput = document.querySelector("#btnClear");
+const numberInput = [
+    {btn: document.querySelector("#btn0"), value: 0},
+    {btn: document.querySelector("#btn1"), value: 1},
+    {btn: document.querySelector("#btn2"), value: 2},
+    {btn: document.querySelector("#btn3"), value: 3},
+    {btn: document.querySelector("#btn4"), value: 4},
+    {btn: document.querySelector("#btn5"), value: 5},
+    {btn: document.querySelector("#btn6"), value: 6},
+    {btn: document.querySelector("#btn7"), value: 7},
+    {btn: document.querySelector("#btn8"), value: 8},
+    {btn: document.querySelector("#btn9"), value: 9},
+    {btn: document.querySelector("#btnAdd"), value: " + "},
+    {btn: document.querySelector("#btnSubtract"), value: " - "},
+    {btn: document.querySelector("#btnMultiply"), value: " x "},
+    {btn: document.querySelector("#btnDivide"), value: " / "}
 ];
 
-numberInput.forEach((btn) => btn.num.addEventListener("click", () => {
-    display.textContent += btn.value;
+numberInput.forEach((button) => button.btn.addEventListener("click", () => {
+    display.textContent += button.value;
+    operationFromDisplay = display.textContent;
 }));
 
-let operatorInput = [
-    {operator: document.querySelector("#btnAdd"), value: "+"},
-    {operator: document.querySelector("#btnSubtract"), value: "-"},
-    {operator: document.querySelector("#btnMultiply"), value: "x"},
-    {operator: document.querySelector("#btnDivide"), value: "/"}
-];
-
-operatorInput.forEach((btn) => btn.operator.addEventListener("click", () => {
-    display.textContent += btn.value;
-}));
-
-let clearInput = document.querySelector("#btnClear")
 clearInput.addEventListener("click", () => display.textContent = "");
+
+equalInput.addEventListener("click", () => {
+    let arrayFromDisplay = operationFromDisplay.split(" ");
+    numberFirst = parseInt(arrayFromDisplay[0]);
+    numberSecond = parseInt(arrayFromDisplay[2]);
+    operator = arrayFromDisplay[1];
+    operate(numberFirst, numberSecond, operator);
+});
+
+function operate (numOne, numTwo, operator) {
+    
+    let result = () => {
+    switch (operator) {
+        case "+": return add(numOne, numTwo); break;
+        case "-": return subtract(numOne, numTwo); break;
+        case "x": return multiply(numOne, numTwo); break;
+        case "/": return divide(numOne, numTwo); break;
+    }
+    }
+    return display.textContent = result();
+}
